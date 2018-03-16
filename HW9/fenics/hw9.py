575944f3692edbf69b5b0d619da1ee5043ddfca8
Jon <jon-doucette@hotmail.com>
2018-03-15 20:16:52 -0700

    finished hw9

--- HW9/fenics/hw9.py
+++ HW9/fenics/hw9.py
@@ -8,9 +8,9 @@ from __future__ import print_function
 from fenics import *
 from mshr import *
 import time
+import numpy as np
 
-def heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
-                            fname = 'heat/bwdeuler_theta.pvd', cmp = False):
+def create_heateqn_problem(t0 = 0.0, T = 5.0, dt = 1e-2):
     # Create a geometry and mesh it
     square = Rectangle(Point(0., 0.), Point(1., 1.))
     diskM = Circle(Point(0.5, 0.5), 0.2)
@@ -27,8 +27,12 @@ def heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
     V = FunctionSpace(mesh, 'P', 1)
 
     # Problem data
-    t = t0 # current time
     u0 = interpolate(Constant(20.0), V) # initial temperature
+    T_source = 1.0 # source f is active for the first 'T_source' seconds
+
+    # Parameters of the time-stepping scheme
+    t = t0 # current time
+    tsteps = int(round(T/dt)) # number of time steps (round to ensure integer)
 
     # Refer to Theorem 2.3.25 to decide upon a sensible degree for the interpolation
     # of the source term f below
@@ -39,12 +43,57 @@ def heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
     #   functions O(h²) in 2D, if we take an order 1 approximation of f, the error
     #   in approximating f will be O(h²) which is the same order as the error from
     #   integrating the quadratic f*v, which is less than the O(h) error
-    t_source = 1.0 # source f is active for the first 't_source' seconds
     f = Expression("t > tstop ? 0 : 200*exp(-5*x[0]*x[0]-2*x[1]*x[1])",
-                    degree = 1, t = t, tstop = t0 + t_source)
+    degree = 1, t = t, tstop = t0 + T_source)
 
-    # Parameters of the time-stepping scheme
-    tsteps = int(round(T/dt)) # number of time steps (round to ensure integer)
+    return u0, f, V, t, tsteps
+
+
+def heateqn_bwdeuler(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, prnt = True,
+                     save = False, fname = 'heat/bwdeuler_theta.pvd'):
+    # Create problem geometry
+    u0, f, V, t, tsteps = create_heateqn_problem(t0 = 0.0, T = 5.0, dt = 1e-2)
+
+    # Define the variational problem for the Backward Euler scheme
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    B = u*v*dx + dt*a*dot(grad(u), grad(v))*dx
+
+    # Export the initial data
+    u = Function(V, name='Temperature')
+    u.assign(u0)
+    if save:
+        results = File('heat/backwardEuler.pvd')
+        results << (u, t)
+
+    # Time stepping
+    for k in range(tsteps):
+
+        # Current time
+        t = t0 + (k+1)*dt
+        if prnt:
+            print('Step = ', k+1, '/', tsteps , 'Time =', t)
+
+        # Assemble the right hand side
+        f.t = t
+        L = (u0 + dt*f)*v*dx
+
+        # Compute the solution
+        solve(B == L, u)
+        if save:
+            results << (u, t)
+
+        # Update
+        u0.assign(u)
+
+    # Return Solution
+    return u
+
+def heateqn_thetamethod(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
+                            save = False, fname = 'heat/bwdeuler_theta.pvd',
+                            prnt = True, cmp = False):
+    # Create problem geometry
+    u0, f, V, t, tsteps = create_heateqn_problem(t0 = 0.0, T = 5.0, dt = 1e-2)
 
     # Define the variational problem
     u = TrialFunction(V)
@@ -56,8 +105,9 @@ def heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
     # Export the initial data
     u = Function(V, name='Temperature')
     u.assign(u0)
-    results = File(fname)
-    results << (u, t)
+    if save:
+        results = File(fname)
+        results << (u, t)
 
     # Time stepping
     f.t = t0
@@ -65,7 +115,8 @@ def heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
 
         t_last = t0 + k*dt # Last time
         t_curr = t0 + (k+1)*dt # Current time
-        print('Step = ', k+1, '/', tsteps , 'Time =', t_curr)
+        if prnt:
+            print('Step = ', k+1, '/', tsteps , 'Time =', t_curr)
 
         # Assemble the right hand side
         #   -> Currently, f.t == t_last from the last loop itereration
@@ -80,61 +131,53 @@ def heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
 
         # Compute the solution
         solve(B == L, u)
-        results << (u, t_curr)
+        if save:
+            results << (u, t_curr)
 
         # Update
         u0.assign(u)
 
-    # ------------------------------------------------------------------------ #
-    # For comparing with BE for theta = 1.0
-    # ------------------------------------------------------------------------ #
     if cmp:
-        u0 = interpolate(Constant(20.0), V) # initial temperature
-        U_BE = Function(V, name='Temperature')
-        U_BE.assign(u0)
-        results = File('heat/bwdeuler.pvd')
-        results << (U_BE, t)
+        # Compare the norm of the difference between the solution calculated
+        # with the theta-method and with the backward euler method
+        u_BE = heateqn_bwdeuler(t0 = t0, T = T, dt = dt, a = a,
+                                prnt = prnt, save = save)
+        print('\n')
+        print('u_th_max = ', np.max(np.abs(u.vector().get_local())))
+        print('u_BE_max = ', np.max(np.abs(u_BE.vector().get_local())))
+        print('||u-u_BE||_L2 = ', errornorm(u, u_BE, 'L2'))
+        print('\n')
 
-        # Time stepping
-        for k in range(tsteps):
-
-            # Current time
-            t = t0 + (k+1)*dt
-            print('Step = ', k+1, '/', tsteps , 'Time =', t)
-
-            # Assemble the right hand side
-            f.t = t
-            L = (u0 + dt*f)*v*dx
-
-            # Compute the solution
-            solve(B == L, U_BE)
-            results << (U_BE, t)
-
-            # Update
-            u0.assign(U_BE)
-
-        print('\n', '||u-U_BE||_L2 = ', errornorm(u, U_BE, 'L2'), '\n')
+# ---------------------------------------------------------------------------- #
+# Stricter log level to avoid so much printing
+# ---------------------------------------------------------------------------- #
+set_log_level(30)
 
 # ---------------------------------------------------------------------------- #
 # Q2(b): theta-method - backward euler consistency check with Q2(a)
 # ---------------------------------------------------------------------------- #
-heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
-                        fname = 'heat/thetaMethodBwdEuler.pvd', cmp = True)
+heateqn_thetamethod(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 1.0,
+                    fname = 'heat/thetaMethodBwdEuler.pvd',
+                    save = True, cmp = True, prnt = True)
 
 # ---------------------------------------------------------------------------- #
 # Q2(b): theta-method - forward euler and Crank-Nicolson
 # ---------------------------------------------------------------------------- #
-heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 0.0,
-                        fname = 'heat/thetaMethodFwdEuler.pvd')
+heateqn_thetamethod(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 0.0,
+                    fname = 'heat/thetaMethodFwdEuler.pvd',
+                    save = True, cmp = True, prnt = True)
 
-heatEqnHomogNeumannBdry(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 0.5,
-                        fname = 'heat/thetaMethodCrankNicolson.pvd')
+heateqn_thetamethod(t0 = 0.0, T = 5.0, dt = 1e-2, a = 0.1, theta = 0.5,
+                    fname = 'heat/thetaMethodCrankNicolson.pvd',
+                    save = True, cmp = True, prnt = True)
 
 # ---------------------------------------------------------------------------- #
 # Q2(c): theta-method - forward euler, shorter duration T and timestep dt
 # ---------------------------------------------------------------------------- #
-heatEqnHomogNeumannBdry(t0 = 0.0, T = 0.1, dt = 1.25e-4, a = 0.1, theta = 0.0,
-                        fname = 'heat/thetaMethodFwdEuler__T_0p1__dt_0p000125.pvd')
+heateqn_thetamethod(t0 = 0.0, T = 0.1, dt = 1.25e-4, a = 0.1, theta = 0.0,
+                    fname = 'heat/thetaMethodFwdEuler__T_0p1__dt_0p000125.pvd',
+                    save = True, cmp = True, prnt = True)
 
-heatEqnHomogNeumannBdry(t0 = 0.0, T = 0.1, dt = 1.00e-4, a = 0.1, theta = 0.0,
-                        fname = 'heat/thetaMethodFwdEuler__T_0p1__dt_0p0001.pvd')
+heateqn_thetamethod(t0 = 0.0, T = 0.1, dt = 1.00e-4, a = 0.1, theta = 0.0,
+                    fname = 'heat/thetaMethodFwdEuler__T_0p1__dt_0p0001.pvd',
+                    save = True, cmp = True, prnt = True)
